2776d72e569cc295ae451cdc616ddbdab48fab8e
Van <van>
2024-01-10 17:55:15 +0700

    editfor tele

--- index.js
+++ index.js
@@ -7,49 +7,51 @@ const browser = await puppeteer.launch({
   headless: false,
   args: ["--start-maximized"],
 });
-const page = await browser.newPage();
+let page = await browser.newPage();
 
 page.setDefaultNavigationTimeout(120000);
-const url = 'https://seller-us.tiktok.com/product/manage?shop_region=US&tab=failed'
-await page.goto(url, { waitUntil: "domcontentloaded" });
+const url = " https://web.telegram.org/a/#-1001144740753";
 
-await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+await page.goto(url, { waitUntil: "load" });
 
-const lastPost = await page.waitForSelector("[id$=hplChiTiet_0]");
+await page.waitForNavigation({ waitUntil: "load" });
+console.log("done wait for navigation");
+const phone = await page.waitForSelector("button");
+console.log("loaded", phone);
+await page.click("button");
+// const text = await page.evaluate((el) => {
+//   return el.textContent;
+// }, lastPost);
+// const title = await page.evaluate(() => {
+//   return document.querySelector(".entry h2").textContent;
+// });
 
-const text = await page.evaluate((el) => {
-  return el.textContent;
-}, lastPost);
-const title = await page.evaluate(() => {
-  return document.querySelector(".entry h2").textContent;
-});
+// const myFrame = page
+//   .frames()
+//   .find((frame) => frame.name().includes(input.frameName));
 
-const myFrame = page
-  .frames()
-  .find((frame) => frame.name().includes(input.frameName));
+// await myFrame.$eval(
+//   input.selector,
+//   (el, content) => {
+//     el.innerHTML = content;
+//   },
+//   input.content
+// );
 
-await myFrame.$eval(
-  input.selector,
-  (el, content) => {
-    el.innerHTML = content;
-  },
-  input.content
-);
+// await page.setContent(html);
 
-await page.setContent(html);
+// await dialog.dismiss();
 
-await dialog.dismiss();
+// await page.click('[id$="TroVe"]');
 
-await page.click('[id$="TroVe"]');
+// const fields = contentFn.createFields(postID);
+// let page = await formFn.sendForm({ browser, url, fields, fileName });
+// let afterSendFn = whichAfterSendFn(url);
+// const pageDoneAfterSend = await afterSendFn({ page, fileName, url });
+// if (pageDoneAfterSend) {
+//   await pageDoneAfterSend.close();
+//   console.log(`Sent - ${helpersJs.getHomePage(url)} - ${postTitle}`);
+// }
 
-const fields = contentFn.createFields(postID);
-let page = await formFn.sendForm({ browser, url, fields, fileName });
-let afterSendFn = whichAfterSendFn(url);
-const pageDoneAfterSend = await afterSendFn({ page, fileName, url });
-if (pageDoneAfterSend) {
-  await pageDoneAfterSend.close();
-  console.log(`Sent - ${helpersJs.getHomePage(url)} - ${postTitle}`);
-}
-
-await browser.close();
-process.exit();
+// await browser.close();
+// process.exit();
