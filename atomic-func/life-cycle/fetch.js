b4b002ab477f6f24bcaf472859bdbac25f901c09
Van <van>
2024-01-11 17:57:17 +0700

    Fix goto of puppeteer

--- atomic-func/life-cycle/fetch.js
+++ atomic-func/life-cycle/fetch.js
@@ -1,11 +1,18 @@
+import debug from "../debug/debug.js";
 // wait for fetch response for too long  without receiving any response, we have to abort it. Because Using a promise race solution will leave the request hanging and still consume bandwidth in the background and lower the max allowed concurrent request being made while it's still in process.
-async function fetchOrAbort(milisecond) {
+async function fetchOrAbort(url, milisecond) {
   const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), milisecond);
+  const timeoutId = setTimeout(() => {
+    debug.err(
+      `Can't access ${url}, waited for ${milisecond}ms but no response`
+    );
+    return controller.abort();
+  }, milisecond);
 
   const res = await fetch(url, { signal: controller.signal });
   if (res) {
     clearTimeout(timeoutId);
+    return res;
   }
 }
 
--- atomic-func/puppeteer/navigate.js
+++ atomic-func/puppeteer/navigate.js
@@ -3,11 +3,15 @@ import debug from "../debug/debug.js";
 
 async function goto(page, url) {
   try {
-    const res = await fetch(url);
+    const res = await fetchOrAbort(url, 30000);
 
-    if (res.status == 200) {
+    if (res.ok) {
       await page.goto(url);
       return page;
+    } else {
+      debug.err(
+        `Can't access ${url}, error: ${res.status} - ${res.statusText}`
+      );
     }
   } catch (e) {
     debug.err(`Can't access ${url}, error: ${e}`);
