b72ceb3a6bceb23d3b7e2ed6b52f98720a3b38c4
Van <van>
2024-01-12 08:49:03 +0700

    update

--- atomic-func/life-cycle/fetch.js
+++ atomic-func/life-cycle/fetch.js
@@ -1,5 +1,8 @@
 import debug from "../debug/debug.js";
 // wait for fetch response for too long  without receiving any response, we have to abort it. Because Using a promise race solution will leave the request hanging and still consume bandwidth in the background and lower the max allowed concurrent request being made while it's still in process.
+
+// const url = "https://web.telegraddddm.org/a/#-1001144740753";
+// fetchOrAbort(url, 10000);
 async function fetchOrAbort(url, milisecond) {
   const controller = new AbortController();
   const timeoutId = setTimeout(() => {
@@ -9,10 +12,15 @@ async function fetchOrAbort(url, milisecond) {
     return controller.abort();
   }, milisecond);
 
-  const res = await fetch(url, { signal: controller.signal });
-  if (res) {
+  try {
+    const res = await fetch(url, { signal: controller.signal });
+    if (res) {
+      return res;
+    }
+  } catch (e) {
+    debug.err(`Can't access ${url}, error: ${e}`);
+  } finally {
     clearTimeout(timeoutId);
-    return res;
   }
 }
 
