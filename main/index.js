20cfad7d6caef19748b9f2b9300ebe05b80c7c4e
Van <van>
2024-03-21 15:11:45 +0700

    update

--- main/index.js
+++ main/index.js
@@ -1,5 +1,4 @@
-import getAppRoot from "../get-app-root.js";
-import main from "./main.js";
+import getAppRoot from "../get-app-root.js"; 
 import debug from "../atomic-func/debug/debug.js";
 import checkInput from "./sub/check-input.js";
 import chalk from "chalk";
@@ -10,14 +9,15 @@ try {
   main();
 } catch (e) {
   debug.err(e);
-  return;
+ 
 } finally {
   // Close db if needed
-  // exit process. When you run it as a script and it hangs when "done", it means node still has callbacks egistered waiting for events. Node doesn't know that those events won't fire anymore. You can either just call process.exit()  if you know it's time to exit, or you can explicitly  close/unbind/disconnect everything (network connections, db connections,   etc). If you properly close everything, node should then exit.
+  // exit process. When you run it as a script and it hangs when "done", it means node still has callbacks registered waiting for events. Node doesn't know that those events won't fire anymore. You can either just call process.exit()  if you know it's time to exit, or you can explicitly  close/unbind/disconnect everything (network connections, db connections,   etc). If you properly close everything, node should then exit.
   process.exit();
 }
 
 function main() {
-  console.log(chalk.bold.green("hi"));
-  console.log(x)
+ 
+  console.log(chalk.bold.green("Starting"));
+ 
 }
--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 - `npm start`
 
 # Logging
-
+## Log file location
+*output/log.txt*
+## Function to log to console
 
 import debug from *atomic-func* and write those functions in the code
 - `debug.dev`: This command will log to console if the environment variable DEBUG = "dev"
@@ -10,8 +12,8 @@ import debug from *atomic-func* and write those functions in the code
         - open PowerShell
         - type `$env:DEBUG="dev"`
         - package "debug" doens't work with variables in *.env* file
-- `debug.err`: This will always log to the console and write to *output/log.txt*
-- `debug.clearLog`: This will clear file *output/log.txt*
+- `debug.err`: This will always log to the console and write to log file
+- `debug.clearLog`: This will clear log file
 
 
 # Fail fast, success later
